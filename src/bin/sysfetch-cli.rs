7414a22d4f478afa06358c3d38626024f5e6d73a
Jacob Hrbek <kreyren@fsfe.org>
2020-12-28 06:25:28 +0100

    Update

--- src/bin/sysfetch-cli.rs
+++ src/bin/sysfetch-cli.rs
@@ -1,41 +1,34 @@
-//! # sysfetch-cli
-//!
-//! Standalone Command LIne (CLI) frontend for fetching a userland of an OS into a desired directory using sysfetch crate's functions
+///! # sysfetch-cli
+///!
+///! Standalone Command LIne (CLI) frontend for fetching a userland of an OS into a desired directory using sysfetch crate's functions
 
 // FIXME-QA(Krey): Consider implementing SNAFU <https://github.com/shepmaster/snafu> for better errors
 // FIXME-BRAINSTORM(Krey): Consult with the torproject how to write software that is friendly to onion-services
 
 /// Function to get userland of Exherbo Linux in specified directory
 // TODO(Krey): See chapter in rustbook on slices as `str` is a slice thus we need `&str`
+// DNC(Krey): Working on this, contrib of shims appreciated
 sysfetch_exherbo(destDir: &str) {
 	unimplemented!("Not implemented");
 
-	// DNM-CROSS(Krey): Has to work on windows
-	// DNM-CROSS(Krey): Has to work on darwin
-	// DNM-CROSS(Krey): Has to work on redox
-	// DNM-CROSS(Krey): Has to work on freebsd
-	// DNM-CROSS(Krey): Has to work on react
-	// FIXME-ASK(Krey): If there is a crate that standardizes paths per platform, if not create it
-	match userland {
-		gnu => {
-			// DNM(Krey): Expected to point to /home/<USER>
-			let STD_CACHE_DIR = "/home/kreyren/.cache/",
-		}
-		_ => die(tbd; "Userland '{:1}' is not recognized", userland)
-	}
+	// Download the tarball
+	fetch!("https://dev.exherbo.org/stages/exherbo-x86_64-pc-linux-gnu-current.tar.xz")
+		// DNM(Krey): Do not hard-code get the checksum from <https://dev.exherbo.org/stages>
+		.sha256sum("f6bb1b9bf7067b193494de9b8a0f9c845f45461425646aa178bd3b2b2251836e")
+		// FIXME-QA(Krey): Use destDir here
+		.destination(cache_dir + project_name + file_name)
 
-	// DNM(Krey): Capture this from https://dev.exherbo.org/stages/sha256sum
-	let checksum = "f6bb1b9bf7067b193494de9b8a0f9c845f45461425646aa178bd3b2b2251836e",
-
-	// DNM-PSEUDO(Krey): Expression
-	if ! shasum(checksum, cacheDir + "sysfetch/exherbo-x86_64-pc-linux-gnu-current.tar.xz") {
-		// DNM-PSEUDO(Krey): Download is not defined, find create
-		download!("https://dev.exherbo.org/stages/exherbo-x86_64-pc-linux-gnu-current.tar.xz"; matches.value_of("TARGET").unwrap_or(STD_CACHE_DIR + "sysfetch"))
-	}
+	// Extract the tarball
+	extract!(cache_dir + project_name + "exherbo-x86_64-pc-linux-gnu-current.tar.xz")
+		// DNM(Krey): Determine the default path for extracting the tarball
+		.destination(matches.value_of("TARGET").unwrap_or("TBD"))
 }
 
 extern crate clap;
 use clap::Clap;
+
+extern crate directories_next;
+use directories_next::BaseDirs;
 
 fn main() {
 	let matches = App::new("sysfetch-cli")
@@ -51,7 +44,7 @@ fn main() {
 				// FIXME-DOCS(Krey): Not descriptive enough
 				.about("Sets a custom config file"))
 		// DNM-QA(Krey): Check if this works for the option
-		#[option(torrent)]
+		#[cfg(feature = "torrent")]
 		.arg(
 			Arg::new("torrent")
 				// FIXME(Krey): Is -t a good option here?
@@ -100,8 +93,9 @@ fn main() {
 
 	match let variant = matches.value_of("VARIANT").to_lowercase() {
 		exherbo => sysfetch_exherbo(),
+		debian => sysfetch_debian(),
 		_ => {
-			// FIXME-CONTRIB(Krey): Implement handling of strings to die crate to handle cross-platform
+			// DNC-CONTRIB(Krey): Implement handling of strings to die crate to handle cross-platform
 			die(synerr; "Variant '{:1}' is not recognized", variant)
 		}
 	}
